5ec50c66bdce0f442c37f65f7427f3d1ae5ca174
Lương Thế Hoàng <hoang.luong010206@hcmut.edu.vn>
2026-05-31 23:18:55 +0700

    chore: fix unused import

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,5 +1,4 @@
 // Vị trí: src/routes/ProtectedRoute.tsx
-import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthContext } from '../shared/context/AuthContext';
 
--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,5 +1,4 @@
 // Vị trí: src/routes/PublicRoute.tsx
-import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthContext } from '../shared/context/AuthContext';
 
