05019bdaf61786804bc4e780d6ad34db0f24e3ba
Lương Thế Hoàng <hoang.luong010206@hcmut.edu.vn>
2026-06-01 20:45:49 +0700

    chore: update for demo

--- src/features/LessonDetail/api/userProgressApi.ts
+++ src/features/LessonDetail/api/userProgressApi.ts
@@ -3,7 +3,7 @@ import axiosClient from '../../../shared/api/axiosClient';
 export const userProgressApi = {
   updateLessonProgress: async (userId: string) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const response: any = await axiosClient.patch('/users/update-lesson', {
+    const response: any = await axiosClient.patch('/auth/update-lesson', {
       userId: userId,
     });
     return response;
